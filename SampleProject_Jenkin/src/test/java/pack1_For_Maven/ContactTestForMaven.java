5218364ea21d3d7d3031c0945fd9a3bf7ced63e6
BShankar <101615706+Bhimashankarsg@users.noreply.github.com>
2024-11-13 10:29:46 +0530

    First push_jenkin

---
+++ SampleProject_Jenkin/src/test/java/pack1_For_Maven/ContactTestForMaven.java
@@ -0,0 +1,26 @@
+package pack1_For_Maven;
+
+import org.testng.annotations.Test;
+
+public class ContactTestForMaven {
+@Test
+	public void createcontactTest()
+	{
+	String URL=System.getProperty("url");
+	String BROWSER=System.getProperty("browser","chrome");
+	String USERNAME=System.getProperty("username");
+	String PASSWORD=System.getProperty("password");
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
+		System.out.println("Execute createcontactTest");
+	}
+	@Test
+	public void modifycontactTest()
+	{
+		System.out.println("Execute modifycontactTest");
+	}
+}
---
+++ SampleProject_Jenkin/src/test/java/pack2_For_Maven/OrgTestForMaven.java
@@ -0,0 +1,19 @@
+package pack2_For_Maven;
+
+import org.testng.annotations.Test;
+
+public class OrgTestForMaven {
+	
+@Test
+	public void createOrgTest()
+	{
+	
+	
+		System.out.println("Execute createOrgTest");
+	}
+	@Test
+	public void modifyOrgtTest()
+	{
+		System.out.println("Execute modifyOrgtTest");
+	}
+}
